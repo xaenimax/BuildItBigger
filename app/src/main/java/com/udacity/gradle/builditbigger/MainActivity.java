637ec6dbbb301277e24bd41e247d56cbd9c7094f
xaenimax <xaenimax@users.noreply.github.com>
2018-05-13 18:49:46 +0200

    adding android library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -42,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, JokeDispenser.getAJoke(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, JokeDispenser.getAJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, ShowJokeActivity.class);
+
     }
 
 
