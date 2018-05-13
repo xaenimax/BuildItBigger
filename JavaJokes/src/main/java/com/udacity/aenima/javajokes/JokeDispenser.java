cb0194c083c9f2c713ba490a38e728eaa20bbbc1
xaenimax <xaenimax@users.noreply.github.com>
2018-05-13 17:40:34 +0200

    added java library and dependencies

---
+++ JavaJokes/src/main/java/com/udacity/aenima/javajokes/JokeDispenser.java
@@ -0,0 +1,9 @@
+package com.udacity.aenima.javajokes;
+
+public class JokeDispenser {
+    private final static String ONE_LINE_JOKE = "To err is human - and to blame it on a computer is even more so.";
+
+    public static String getAJoke(){
+        return ONE_LINE_JOKE;
+    }
+}
