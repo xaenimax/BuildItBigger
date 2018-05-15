044bb188676457ec1d51ff59b97108805868ba99
xaenimax <xaenimax@users.noreply.github.com>
2018-05-15 14:45:06 +0200

    added a functional test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/testing/RetrieveJokeTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/testing/RetrieveJokeTest.java
@@ -1,5 +1,38 @@
 package com.udacity.gradle.builditbigger.testing;
 
-public class Tests {
-    
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.udacity.gradle.builditbigger.MainActivity;
+import com.udacity.gradle.builditbigger.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsNot.not;
+
+@RunWith(AndroidJUnit4.class)
+public class RetrieveJokeTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void jokeIsNotEmpty(){
+        //Find view and perform action
+        onView(withId(R.id.joke_btn)).perform(click());
+
+        //check
+        onView(withId(R.id.joke_text_tv)).check(matches(not(withText(""))));
+
+    }
+
+
+
 }
