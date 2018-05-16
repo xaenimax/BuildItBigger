008ab3b9e0b19bcd32d4eb0299f2dc0672699ff7
xaenimax <xaenimax@users.noreply.github.com>
2018-05-16 18:41:52 +0200

    added insterstitial ad

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
@@ -1,25 +1,38 @@
-package com.udacity.gradle.builditbigger;
+package com.udacity.gradle.builditbigger.free;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.udacity.aenima.androidjokes.Constants;
 import com.udacity.aenima.androidjokes.ShowJokeActivity;
 import com.udacity.aenima.javajokes.JokeDispenser;
+import com.udacity.gradle.builditbigger.R;
+import com.udacity.gradle.builditbigger.RetrieveJokeAsyncTask;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        mInterstitialAd.setAdListener(mAdListener);
     }
 
 
@@ -52,11 +65,25 @@ public class MainActivity extends AppCompatActivity {
         //Intent intent = new Intent(this, ShowJokeActivity.class);
         //intent.putExtra(Constants.JOKE_STRING_EXTRA, JokeDispenser.getAJoke());
         //startActivity(intent);
-        //Step 3
-        RetrieveJokeAsyncTask task = new RetrieveJokeAsyncTask();
-        task.execute(this);
+        //show interstitial ad
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.i(this.getClass().getName(), "Failed to load Interstitial Ad");
+        }
+
 
     }
 
+    //AdListener
+    AdListener mAdListener = new AdListener(){
+        @Override
+        public void onAdClosed() {
+            super.onAdClosed();
+            //Step 3
+            RetrieveJokeAsyncTask task = new RetrieveJokeAsyncTask();
+            task.execute(MainActivity.this);
+        }
+    };
 
 }
---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivity.java
@@ -0,0 +1,64 @@
+package com.udacity.gradle.builditbigger.paid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.udacity.aenima.androidjokes.Constants;
+import com.udacity.aenima.androidjokes.ShowJokeActivity;
+import com.udacity.aenima.javajokes.JokeDispenser;
+import com.udacity.gradle.builditbigger.R;
+import com.udacity.gradle.builditbigger.RetrieveJokeAsyncTask;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+        //Step 1
+        //Toast.makeText(this, JokeDispenser.getAJoke(), Toast.LENGTH_SHORT).show();
+        //Step 2
+        //Intent intent = new Intent(this, ShowJokeActivity.class);
+        //intent.putExtra(Constants.JOKE_STRING_EXTRA, JokeDispenser.getAJoke());
+        //startActivity(intent);
+        //Step 3
+        RetrieveJokeAsyncTask task = new RetrieveJokeAsyncTask();
+        task.execute(this);
+
+    }
+
+
+}
